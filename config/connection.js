cc96639112dceaee68ab468726ec71b05952dccf
Optical23 <aaroneferguson@gmail.com>
2022-01-19 23:21:59 -0600

    set up handlbars

--- config/connection.js
+++ config/connection.js
@@ -4,10 +4,14 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.SDB_URL);
 } else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  sequelize = new Sequelize(
+    process.env.DB_NAME, 
+    process.env.DB_USER, 
+    process.env.DB_PASSWORD, 
+    {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
